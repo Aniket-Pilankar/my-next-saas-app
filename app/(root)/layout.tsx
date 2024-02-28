38ddafbfe5b1c0a44143f84f83e7c2fb839decd4
Aniket Pilankar <aniketpilankar1998@gmail.com>
2024-02-28 10:54:43 +0530

    (feat):add mobile view for sidebar and mondoDb connection

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,11 @@
+import MobileViewSidebar from "@/components/shared/MobileViewSidebar";
 import Sidebar from "@/components/shared/Sidebar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
       <Sidebar />
+      <MobileViewSidebar />
 
       <div className="root-container">
         <div className="wrapper">{children}</div>
---
+++ docs/mongoose.md
@@ -0,0 +1,2 @@
+1. Mongoose cache connection
+   (https://www.mongodb.com/community/forums/t/next-js-issue-with-multiple-unclosed-connections/10003)
