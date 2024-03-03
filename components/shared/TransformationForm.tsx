37828e01d8692c2dbd5bce6b2ebd2eb8315a67fb
Aniket Pilankar <aniketpilankar1998@gmail.com>
2024-03-03 10:49:10 +0530

    (feat): add Transformation image component and image server actions performing crud operation

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -34,6 +34,10 @@ import { useEffect, useState, useTransition } from "react";
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
 import { useRouter } from "next/navigation";
 import MediaUploader from "./MediaUploader";
+import TransformedImage from "./TransformedImage";
+import { addImage, updateImage } from "@/lib/actions/image.actions";
+import { getCldImageUrl } from "next-cloudinary";
+import { InsufficientCreditsModal } from "./InsufficientCreditsModal";
 
 export const formSchema = z.object({
   title: z.string(),
@@ -80,6 +84,70 @@ const TransformationForm = ({
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof formSchema>) {
     console.log("values:", values);
+
+    setIsSubmitting(true);
+
+    if (data || image) {
+      const transformationUrl = getCldImageUrl({
+        width: image?.width,
+        height: image?.height,
+        src: image?.publicId,
+        ...transformationConfig,
+      });
+
+      const imageData = {
+        title: values.title,
+        publicId: image?.publicId,
+        transformationType: type,
+        width: image?.width,
+        height: image?.height,
+        config: transformationConfig,
+        secureURL: image?.secureURL,
+        transformationURL: transformationUrl,
+        aspectRatio: values.aspectRatio,
+        prompt: values.prompt,
+        color: values.color,
+      };
+
+      if (action === "Add") {
+        try {
+          const newImage = await addImage({
+            image: imageData,
+            userId,
+            path: "/",
+          });
+
+          if (newImage) {
+            form.reset();
+            setImage(data);
+            router.push(`/transformations/${newImage._id}`);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+      if (action === "Update") {
+        try {
+          const updatedImage = await updateImage({
+            image: {
+              ...imageData,
+              _id: data._id,
+            },
+            userId,
+            path: `/transformations/${data._id}`,
+          });
+
+          if (updatedImage) {
+            router.push(`/transformations/${updatedImage._id}`);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
+
+    setIsSubmitting(false);
   }
 
   const onSelectFieldHandler = (
@@ -239,6 +307,17 @@ const TransformationForm = ({
               />
             )}
           />
+
+          <TransformedImage
+            image={image}
+            type={type}
+            title={form.getValues().title}
+            isTransforming={isTransforming}
+            setIsTransforming={setIsTransforming}
+            transformationConfig={transformationConfig}
+          />
+        </div>
+
         <div className="flex flex-col gap-4">
           <Button
             type="button"
--- lib/utils.ts
+++ lib/utils.ts
@@ -42,6 +42,27 @@ export const getImageSize = (
   return image?.[dimension] || 1000;
 };
 
+// DOWNLOAD IMAGE
+export const download = (url: string, filename: string) => {
+  if (!url) {
+    throw new Error("Resource URL not provided! You need to provide one");
+  }
+
+  fetch(url)
+    .then((response) => response.blob())
+    .then((blob) => {
+      const blobURL = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = blobURL;
+
+      if (filename && filename.length)
+        a.download = `${filename.replace(" ", "_")}.png`;
+      document.body.appendChild(a);
+      a.click();
+    })
+    .catch((error) => console.log({ error }));
+};
+
 export const deepMergeObjects = (obj1: any, obj2: any) => {
   if (obj2 === null || obj2 === undefined) {
     return obj1;
