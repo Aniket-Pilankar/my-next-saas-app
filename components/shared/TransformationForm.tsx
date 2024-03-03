9900892f44a739e704db87b3ece22580aa4e8e2f
Aniket Pilankar <aniketpilankar1998@gmail.com>
2024-03-03 09:07:05 +0530

    (feat):integrate cloudinary and add upload image feature

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -33,6 +33,7 @@ import { CustomField } from "./CustomField";
 import { useEffect, useState, useTransition } from "react";
 import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
 import { useRouter } from "next/navigation";
+import MediaUploader from "./MediaUploader";
 
 export const formSchema = z.object({
   title: z.string(),
@@ -223,6 +224,21 @@ const TransformationForm = ({
           </div>
         )}
 
+        <div className="media-uploader-field">
+          <CustomField
+            control={form.control}
+            name="publicId"
+            className="flex size-full flex-col"
+            render={({ field }) => (
+              <MediaUploader
+                onValueChange={field.onChange}
+                setImage={setImage}
+                publicId={field.value}
+                image={image}
+                type={type}
+              />
+            )}
+          />
         <div className="flex flex-col gap-4">
           <Button
             type="button"
